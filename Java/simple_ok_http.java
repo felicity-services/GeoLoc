9249caa3e7fea792090a1078d476c85c80c422d5
Oussema DAOUD <oussema.daoud@gmail.com>
2018-07-14 16:57:32 +0200

    Update simple_ok_http.java

--- Java/simple_ok_http.java
+++ Java/simple_ok_http.java
@@ -4,8 +4,6 @@ Request request = new Request.Builder()
   .url("https://api.geoloc.felicity-services.com/json/52.210.46.250")
   .get()
   .addHeader("ACCESS-KEY", "YOUR_ACCESS_KEY")
-  .addHeader("Cache-Control", "no-cache")
-  .addHeader("Postman-Token", "46029e6b-6588-47b7-b70b-ce05829337d6")
   .build();
 
 Response response = client.newCall(request).execute();

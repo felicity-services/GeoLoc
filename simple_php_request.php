a1247c249d72c601b6bf8b790c3a295b63f2710b
Oussema DAOUD <oussema.daoud@gmail.com>
2018-07-14 16:22:27 +0200

    Update simple_php_request.php

--- simple_php_request.php
+++ simple_php_request.php
@@ -11,7 +11,7 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
-    "ACCESS-KEY: ***"
+    "ACCESS-KEY: YOUR_ACCESS_KEY"
   ),
 ));
 
